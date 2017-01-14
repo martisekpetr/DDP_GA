63c62261857ed8f3f63d059a6ad26ed2d8dcaec4
Petr Martisek <martisekpetr@gmail.com>
2017-01-14 17:34:48 +0000

    Pridan skript na spusteni EA a vykresleni grafu.

---
+++ script.sh
@@ -0,0 +1,37 @@
+java -cp out/production/DDP_GA/ evolution.doubledigest.DoubleDigest
+
+input=`grep "inputFile" properties/ga-digest.properties | sed "s/prob.inputFile = //"`
+outputDirectory=`grep "outputDirectory" properties/ga-digest.properties | sed "s/xlog.outputDirectory = //"`
+
+
+logFileNames=""
+legendNames=""
+
+files=""
+
+if [[ -d $input ]] 
+then
+	files=$input/*
+
+fi
+
+if [[ -f $input ]] 
+then
+	files=$input
+fi
+
+for file in $files
+do
+	basename="$(basename ${file%.*})"
+
+	legendNames=$legendNames,$basename
+	logFileNames=$logFileNames,$outputDirectory/$basename
+done
+
+echo $legendNames
+echo $logFileNames
+
+bash createGraphs.sh -logFileNames $logFileNames -legendNames $legendNames
+
+
+
